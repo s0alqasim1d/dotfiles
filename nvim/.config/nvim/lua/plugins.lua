c094422ba127b108c3baa89e0edab56d8d67e165
S. Al-Qasim <bigbrain813@gmail.com>
2022-04-04 05:08:47 +0300

    Updated lspconfig

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -97,7 +97,22 @@ return require('packer').startup(function()
 	use { 'mg979/vim-visual-multi', branch = 'master' }
 	
 	-- Smooth Scroll
-	use 'karb94/neoscroll.nvim'
+	use {	'karb94/neoscroll.nvim',
+			config = function() require('neoscroll').setup({
+				-- All these keys will be mapped to their corresponding default scrolling animation
+				mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+							'<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+				hide_cursor = true,          -- Hide cursor while scrolling
+				stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+				respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+				easing_function = nil,       -- Default easing function
+				pre_hook = nil,              -- Function to run before the scrolling animation starts
+				post_hook = nil,             -- Function to run after the scrolling animation ends
+				performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+			}) end
+	}
 
 	-- nvim buffers/tabs line
 	use {'akinsho/nvim-bufferline.lua', config = function() require("plugins/bufferline") end, requires = 'kyazdani42/nvim-web-devicons'}
@@ -178,9 +193,9 @@ return require('packer').startup(function()
 	-- UltTest
 	use { 'rcarriga/vim-ultest', requires = {'vim-test/vim-test'}, run = ':UpdateRemotePlugins' }
 	-- Neovim LSP-Config
-	use 'neovim/nvim-lspconfig'
+	-- use 'neovim/nvim-lspconfig'
 	-- Neovim LSPContainers
-	use { 'lspcontainers/lspcontainers.nvim', config = function() require'plugins/lspconfig' end }
+	use { 'lspcontainers/lspcontainers.nvim', requires = { 'neovim/nvim-lspconfig' }, config = function() require'plugins/lspconfig' end }
 	-- Neovim LSP Saga
 	use 'glepnir/lspsaga.nvim'
 	-- Lsp Status
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,30 @@
-require'lspconfig'.dockerls.setup {
-  before_init = function(params)
-    params.processId = vim.NIL
-  end,
-  cmd = require'lspcontainers'.command('dockerls'),
-  root_dir = require'lspconfig'.util.root_pattern(".git", vim.fn.getcwd()),
+-- Use a loop to conveniently call "setup" on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = {
+	"bashls",
+	"dockerls",
+	-- "gopls", has problem with env.HOME not being found
+	"html",
+	"jsonls",
+	"pyright",
+	"rust_analyzer",
+	"sumneko_lua",
+	"tsserver",
+	"yamlls"
 }
+
+for _, lsp in pairs(servers) do
+	-- print(lsp)
+	require("lspconfig")[lsp].setup {
+		before_init = function(params)
+			params.processId = vim.NIL
+		end,
+		cmd = require"lspcontainers".command(lsp),
+		root_dir = require"lspconfig".util.root_pattern(".git", vim.fn.getcwd()),
+		on_attach = on_attach,
+		flags = {
+		  -- This will be the default in neovim 0.7+
+		  debounce_text_changes = 150,
+		}
+	}
+end
