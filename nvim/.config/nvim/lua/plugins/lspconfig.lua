229cb21a773f249238e02ea9e4a110d628894747
S. Al-Qasim Dheya <bigbrain813@gmail.com>
2022-04-22 18:07:47 +0300

    Update lspconfig.lua
    Added custom LSP server for nginx config, but even though it isn't throwing an error I can't figure out its problem. I can't even run it locally on a container and get some kind of feedback from the Language Server.

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,10 @@
 local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
 local lspcontainers = require("lspcontainers")
 local capabilities = require"plugins/autocompletion_capabilities"
 -- Use a loop to conveniently call "setup" on multiple servers and
 -- map buffer local keybindings when the language server attaches
+
 local servers = {
 	"bashls",
 	"dockerls",
@@ -14,8 +16,20 @@ local servers = {
 	"rust_analyzer",
 	"sumneko_lua",
 	"tsserver",
-	"yamlls"
+	"yamlls",
 }
+	-- custom language servers because in case they fail, they can't interfere with others loading
+local custom_servers = {
+	["nginx_ls"] = {
+			image = 's0alqasim1d/nginx-ls',
+			default_config = {
+				cmd = "/venv/bin/nginx-language-server -vv",
+				filetypes = { "nginx" },
+				root_dir = lspconfig.util.root_pattern("nginx.conf", ".git", vim.fn.getcwd()),
+			}
+	},
+}
+
 local lsp_settings = {
 	["sumneko_lua"] = {
 		Lua = {
@@ -26,25 +40,22 @@ local lsp_settings = {
 	},
 	["yamlls"] = {
 		yaml = {
-			yamlVersion = '1.2',
-			trace = {
-				server = "verbose"
-			},
+			yamlVersion = "1.2",
+			trace = { server = "verbose" },
 			schemas = {
 --				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.yaml",
 				["kubernetes"] = "/*.k8s.yaml",
---				["C:\\Users\\AlQasim\\Documents\\yamlls-schemas\\kubernetes-json-schema\\master-local\\all.json"] = "/*.k8s.yaml",
 			},
 			schemaDownload = {  enable = true },
 			validate = true,
 		},
-		redhat = {
-			telemetry = {
-				enabled = false
-			}
-		}
+		redhat = { telemetry = { enabled = false } },
 	}
 }
+
+if not configs["nginx_ls"] then
+	configs["nginx_ls"] = { default_config = custom_servers["nginx_ls"].default_config}
+end
 
 for _, lsp in pairs(servers) do
 	-- print(lsp)
@@ -53,6 +64,22 @@ for _, lsp in pairs(servers) do
 			params.processId = vim.NIL
 		end,
 		cmd = lspcontainers.command(lsp),
+		root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
+		on_attach = on_attach,
+		flags = {},
+		capabilities = capabilities,
+		settings = lsp_settings[lsp] or nil
+	}
+end
+
+
+for lsp, lsp_opts in pairs(custom_servers) do
+	-- print(lsp)
+	lspconfig[lsp].setup {
+		before_init = function(params)
+			params.processId = vim.NIL
+		end,
+		cmd = lspcontainers.command(lsp, { image = lsp_opts.image }),
 		root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
 		on_attach = on_attach,
 		flags = {},
