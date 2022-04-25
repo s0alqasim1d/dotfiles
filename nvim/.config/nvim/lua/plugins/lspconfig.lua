799fbfe87c7743864acc685bd6245d5194cc3ffe
S. Al-Qasim Dheya <bigbrain813@gmail.com>
2022-04-25 05:20:40 +0300

    Refactored lspconfig.lua

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -6,22 +6,42 @@ local capabilities = require"plugins/autocompletion_capabilities"
 -- map buffer local keybindings when the language server attaches
 
 local servers = {
-	"bashls",
-	"dockerls",
+	bashls = nil,
+	dockerls = nil,
 	-- "gopls", has problem with env.HOME not being found
-	"html",
-	"jsonls",
-	"powershell_es",
-	"pyright",
-	"rust_analyzer",
-	"sumneko_lua",
-	"tsserver",
-	"yamlls",
-}
-	-- custom language servers because in case they fail, they can't interfere with others loading
-local custom_servers = {
-	["nginx_ls"] = {
-			image = 's0alqasim1d/nginx-ls',
+	html = nil,
+	jsonls = nil,
+	powershell_es = nil,
+	pyright = nil,
+	rust_analyzer = nil,
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = {"vim", "use", "on_attach"},
+				},
+        	},
+		},
+	},
+	tsserver = nil,
+	yamlls = {
+		settings = {
+			yaml = {
+				yamlVersion = "1.2",
+				trace = { server = "verbose" },
+				schemas = {
+	--				https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json = "/*.k8s.yaml",
+					kubernetes = "/*.k8s.yaml",
+				},
+				schemaDownload = {  enable = true },
+				validate = true,
+			},
+			redhat = { telemetry = { enabled = false } },
+		}
+	},
+	-- Custom LSP
+	nginx_ls = {
+			image = "s0alqasim1d/nginx-ls",
 			default_config = {
 				cmd = "/venv/bin/nginx-language-server -vv",
 				filetypes = { "nginx" },
@@ -30,61 +50,22 @@ local custom_servers = {
 	},
 }
 
-local lsp_settings = {
-	["sumneko_lua"] = {
-		Lua = {
-			diagnostics = {
-				globals = {"vim", "use", "on_attach"},
-			},
-        },
-	},
-	["yamlls"] = {
-		yaml = {
-			yamlVersion = "1.2",
-			trace = { server = "verbose" },
-			schemas = {
---				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.yaml",
-				["kubernetes"] = "/*.k8s.yaml",
-			},
-			schemaDownload = {  enable = true },
-			validate = true,
-		},
-		redhat = { telemetry = { enabled = false } },
-	}
-}
-
-if not configs["nginx_ls"] then
-	configs["nginx_ls"] = { default_config = custom_servers["nginx_ls"].default_config}
-end
-
-for _, lsp in pairs(servers) do
-	-- print(lsp)
+for lsp, lsp_opts in pairs(servers) do
+	 --print(lsp)
+	if not lspcontainers.supported_languages[lsp] then
+		print(lsp .. " has config issue")
+		configs[lsp] = { default_config = lsp_opts.default_config}
+	end
 	lspconfig[lsp].setup {
 		before_init = function(params)
 			params.processId = vim.NIL
 		end,
-		cmd = lspcontainers.command(lsp),
+		cmd = lsp_opts.image ~= nil and lspcontainers.command(lsp, { image = lsp_opts.image }) or lspcontainers.command(lsp),
 		root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
 		on_attach = on_attach,
-		flags = {},
+		flags = nil,
 		capabilities = capabilities,
-		settings = lsp_settings[lsp] or nil
-	}
-end
-
-
-for lsp, lsp_opts in pairs(custom_servers) do
-	-- print(lsp)
-	lspconfig[lsp].setup {
-		before_init = function(params)
-			params.processId = vim.NIL
-		end,
-		cmd = lspcontainers.command(lsp, { image = lsp_opts.image }),
-		root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
-		on_attach = on_attach,
-		flags = {},
-		capabilities = capabilities,
-		settings = lsp_settings[lsp] or nil
+		settings = lsp_opts.settings or nil
 	}
 end
 
