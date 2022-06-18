a6655fa29653c67db159669ee84421d97720a905
S. Al-Qasim <bigbrain813@gmail.com>
2022-06-18 20:24:49 +0300

    Added links to wallpapers. Still got to get rid of wallpapers in my repo and have links for them instead.

--- posh/install.ps1
+++ posh/install.ps1
@@ -1,2 +1,10 @@
+# GET WALLPAPERS:
+# https://images.pexels.com/photos/236968/pexels-photo-236968.jpeg?cs=srgb&dl=pexels-pixabay-236968.jpg&fm=jpg&w=3634&h=2423
+# https://images.pexels.com/photos/236296/pexels-photo-236296.jpeg?cs=srgb&dl=pexels-pixabay-236296.jpg&fm=jpg&w=6048&h=3858
+# https://images.pexels.com/photos/448390/pexels-photo-448390.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-448390.jpg&fm=jpg&w=5002&h=3349
+# https://images.pexels.com/photos/459475/pexels-photo-459475.jpeg?cs=srgb&dl=pexels-pixabay-459475.jpg&fm=jpg&w=5040&h=1654
+# https://images.pexels.com/photos/3910141/pexels-photo-3910141.jpeg?cs=srgb&dl=pexels-brett-sayles-3910141.jpg&fm=jpg&w=6048&h=4024
+# https://images.pexels.com/photos/1276429/pexels-photo-1276429.jpeg?cs=srgb&dl=pexels-dids-1276429.jpg&fm=jpg&w=5585&h=3724
+#
 # New-Item -ItemType SymbolicLink -Path ~\.config\nvim -Target ~\dotfiles\nvim\.config\nvim\
 # New-Item -ItemType SymbolicLink -Path ~\<Wherever appdata/local/temp is>\config\nvim -Target ~\dotfiles\nvim\.config\nvim\
